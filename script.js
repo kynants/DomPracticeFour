b3ffaa070bf4c59af7dcb20d377eb8ca410dfbe9
Kynan Stevenson <kynants@gmail.com>
2020-09-09 07:57:57 -0400

    Tried a forth method. Strangly, it works for in the console but not in the document.

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function getAttributes() {
   // Grabs the values of the anchor tag
   var values = document.getElementsByTagName("A")[0];
 
-  // THIRD ATTEMPT
-  values.forEach(element => console.log(element));
+  // FORTH ATTEMPT
+  // https://stackoverflow.com/questions/828311/how-to-iterate-through-all-attributes-in-an-html-element
+  for (var i = 0; i < values.attributes.length; i++) {
+    var attrib = values.attributes[i];
+    document.getElementById("output").innerHTML = attrib.name + ": " + attrib.value + "<br>";
+    console.log(attrib.name + ": " + attrib.value);
+  }
 }
