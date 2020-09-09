f24249788f62adb11af0a0e562038eef665088f7
Kynan Stevenson <kynants@gmail.com>
2020-09-09 07:55:05 -0400

    Tried a second method. No success.

--- script.js
+++ script.js
@@ -2,13 +2,9 @@ function getAttributes() {
   // Grabs the values of the anchor tag
   var values = document.getElementsByTagName("A")[0];
 
-  // FIRST ATTEMPT
-  // Placeholder for output paragraph
-  var outputVar = "";
-  // Loops through the anchor tags property values
-  for (var property in values) {
-    outputVar += values[property] + "<br>";
+  // SECOND ATTEMPT
+  values.forEach(iterator);
+  function iterator(item, index) {
+    document.getElementById("output").innerHTML += index + ":" + item + "<br>";
   }
-  // Spits out the values of the anchor tag through the "value" variable
-  document.getElementById("output").innerHTML = values;
 }
