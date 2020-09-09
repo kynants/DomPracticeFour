7dbafdb48997c75c2f5304d8261d018e9a23587b
Kynan Stevenson <kynants@gmail.com>
2020-09-09 11:41:37 -0400

    Different method, same result.

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function getAttributes() {
   // Grabs the values of the anchor tag
   var values = document.getElementsByTagName("A")[0];
 
-  // FORTH ATTEMPT
+  // FIFTH ATTEMPT
   // https://stackoverflow.com/questions/828311/how-to-iterate-through-all-attributes-in-an-html-element
   for (var i = 0; i < values.attributes.length; i++) {
     var attrib = values.attributes[i];
-    document.getElementById("output").innerHTML = attrib.name + ": " + attrib.value + "<br>";
-    console.log(attrib.name + ": " + attrib.value);
+    if (attrib.specified) {
+      document.getElementById("output").innerHTML = attrib.name + ": " + attrib.value + "<br>"
+      console.log(attrib.name + " = " + attrib.value);
+    }
   }
 }
